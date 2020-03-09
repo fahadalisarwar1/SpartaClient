48b80dccc8d167038c45cdffcc8abc92d0467be2
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2020-03-09 02:55:35 +0100

    update

--- raw_files/P1_client_connection.py
+++ raw_files/P1_client_connection.py
@@ -1,5 +1,13 @@
 import socket
 
-serverIP = "192.168.0.22"
-server_port = 8080
+
+if __name__ == "__main__":
+    serverIP = "192.168.0.22"
+    server_port = 8080
+
+    address = (serverIP, server_port)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
 
