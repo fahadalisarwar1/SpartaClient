aa97fe1de0d3c0c56a3b650e83c24a6295dd88f6
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2020-03-09 04:34:15 +0100

    e

--- raw_files/establishing_connection/P1_client_connection.py
+++ raw_files/establishing_connection/P1_client_connection.py
@@ -2,13 +2,14 @@ import socket
 
 
 if __name__ == "__main__":
-    serverIP = "192.168.0.22"
+    serverIP = "192.168.0.16"
     server_port = 8080
 
     address = (serverIP, server_port)
 
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn.connect(address)
+    print("[+] connection established with server ", serverIP)
     msg = conn.recv(1024)
     print(msg.decode("utf-8"))
 
